e44d6f9fe503d18e9e022b61c67a30489ca3291f
Gordon06060 <gordonz0913@gmail.com>
2025-01-12 09:46:59 -0500

    port2

--- app.py
+++ app.py
@@ -57,5 +57,4 @@ def root():
 def get_map_data():
     return "Hello, World!"
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=os.getenv('PORT', 5000))
+app.run(debug=True, host='0.0.0.0', port=8080)
